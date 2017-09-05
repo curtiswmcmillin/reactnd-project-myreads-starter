2a4aad4371e7eeed8f132a4ea1c753cf797eee27
Curtis McMillin <curtis.w.mcmillin@gmail.com>
2017-09-05 14:36:01 -0500

    refactored into several components using props

---
+++ src/Bookshelf.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book.js'
+
+class Bookshelf extends React.Component {
+    render() {
+        const books = this.props.books
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+                <div className="bookshelf-books">
+                    <ol>
+                        {books.map((book) => (
+                            <li key={book.id}><Book book={book} /></li>                
+                        ))}                   
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bookshelf
